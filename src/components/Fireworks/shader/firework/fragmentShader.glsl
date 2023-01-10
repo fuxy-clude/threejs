17e1b1a56102d5620fa206053425108b9eac7aec
fuxiaoyang <fu.xiaoyang@xinheyun.com>
2023-01-10 10:42:03 +0800

    1

--- src/components/Fireworks/shader/firework/fragmentShader.glsl
+++ src/components/Fireworks/shader/firework/fragmentShader.glsl
@@ -1,11 +1,9 @@
 
 uniform vec3 uColor;
-
-void main() {
-    float distanceLength = distance(gl_PointCoord, vec2(0.5));
-    // distanceLength *=2.0;
-    float strength = 1.0 - distanceLength;
-    strength = step(0.5,strength);
-
+void main(){
+    float distanceToCenter = distance(gl_PointCoord,vec2(0.5));
+    float strength = distanceToCenter*2.0;
+    strength = 1.0-strength;
+    strength = pow(strength,1.5);
     gl_FragColor = vec4(uColor,strength);
 }
